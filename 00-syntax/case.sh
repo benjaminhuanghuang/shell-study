31f1f3ba3a7e8a95295d99b4aecacdabde2edf7b
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2020-12-29 15:21:00 -0800

    update

--- 00-syntax/case.sh
+++ 00-syntax/case.sh
@@ -9,3 +9,12 @@ case "$key" in
     *) echo "Punctuation, whitespace or other";;
 esac
     
+
+case "$AGE" in
+    [1-9]) echo "You are quite young";;
+    [5-9]) echo "Time for elementary school";;
+    1[0-9]) echo "Time of for middle school";;
+    [2-9][0-9]) echo "You are an adult";;
+    *) echo "That doesn't seem to be an age";;
+esac
+        
