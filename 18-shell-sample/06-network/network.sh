c0c7a1a59ed49f8c6ed9781fe06fadfbd3cdee87
benjamin huang <benjaminhuanghuang@gmail.com>
2022-06-23 10:33:42 -0700

    update

--- 18-shell-sample/06-network/network.sh
+++ 18-shell-sample/06-network/network.sh
@@ -1,15 +1,18 @@
 # !/bin/bash
 # watch the file /proc/net/dev
+# usage: sh netwrok.sh eth0
 
 NIC=$1       # netwrok card name
 echo -e "  In ........ Out"
 
 while true; do
-  NEW_IN= $(awk -F'[: ]+' '$0~"'$NIC'"{print $3}' /proc/net/dev)
-  OLD_OUT= $(awk -F'[: ]+' '$0~"'$NIC'"{print $11}' /proc/net/dev)
+  # $0 is the whole row
+  NEW_IN= $(awk '$0~"' $NIC'"{print $2}' /proc/net/dev)
+  OLD_OUT= $(awk '$0~"' $NIC'"{print $10}' /proc/net/dev)
   sleep 1
-  NEW_IN= $(awk -F'[: ]+' '$0~"'$NIC'"{print $3}' /proc/net/dev)
-  NEW_OUT= $(awk -F'[: ]+' '$0~"'$NIC'"{print $11}' /proc/net/dev)
+  
+  NEW_IN= $(awk '$0~"' $NIC'"{print $2}' /proc/net/dev)
+  OLD_OUT= $(awk '$0~"' $NIC'"{print $10}' /proc/net/dev)
   
   IN=$(printf "%.1f%s" "$((($NEW_IN-$OLD_IN)/1024))" "KB/s")
   OUT=$(printf "%.1f%s" "$((($NEW_OUT-$OLD_OUT)/1024))" "KB/s")
